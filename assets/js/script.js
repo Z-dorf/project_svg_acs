3efa989bbd3bbd673305e5faa8dc4886b37bb7fb
Z-dorf <sebastian.zanon2@gmail.com>
2018-05-03 11:08:55 +0200

    add var/class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,102 @@
-var backGround = document.getElementById("BackGround");
+//Variables des élèments svg
+var backGround = document.getElementsByClassName("background");
+var whiteLine = document.getElementsByClassName("Traits");
+var test2 = document.getElementById("Textes");
+var pinkRect = document.getElementsByClassName("pinkRect");
+var lefty = document.getElementsByClassName("lefty");
+var righty = document.getElementsByClassName("righty");
 
-TweenMax.to(".background", 5, {
-    x: 600,
-    rotation: 100,
-    backgroundColor: "purple"
+
+//ACCESS//
+var A1 = document.getElementsByClassName("A1");
+var C2 = document.getElementsByClassName("C2");
+var C3 = document.getElementsByClassName("C3");
+var E4 = document.getElementsByClassName("E4");
+var S5 = document.getElementsByClassName("S5");
+var S6 = document.getElementsByClassName("S6");
+
+
+//CODE//
+var C1 = document.getElementsByClassName("C1");
+var O2 = document.getElementsByClassName("O2");
+var D3 = document.getElementsByClassName("D3");
+var E4 = document.getElementsByClassName("E4");
+
+//SCHOOL//
+var S1 = document.getElementsByClassName("S1");
+var C2 = document.getElementsByClassName("C2");
+var H3 = document.getElementsByClassName("H3");
+var O4 = document.getElementsByClassName("O4");
+var O5 = document.getElementsByClassName("O5");
+var L6 = document.getElementsByClassName("L6");
+
+
+
+TweenMax.to(A1, 5, {
+    opacity: 100
+})
+
+TweenMax.to(C2, 5, {
+    opacity: 100
+})
+
+TweenMax.to(C3, 5, {
+    opacity: 100
+})
+
+TweenMax.to(E4, 5, {
+    opacity: 100
+    })
+
+TweenMax.to(S5, 5, {
+    opacity: 100
+})
+
+TweenMax.to(S6, 5, {
+    opacity: 100
+    })
+
+TweenMax.to(lefty, 5, {
+    y: -1600,
+    // rotation: 100,
+    // fill: "blue"
+ });
+
+ TweenMax.to(righty, 5, {
+    y: 1600,
+    // rotation: 100,
+    // fill: "blue"
+ });
+
+
+
+TweenMax.to(backGround, 5, {
+    // x: -1600,
+    // rotation: 100,
+    // fill: "blue"
+ });
+
+TweenMax.to(pinkRect, 5, {
+    // fill: "blue",
+    x : 600,
+    // rotation: 1500
+}) 
+/* Remarque,
+je peux désormais changer la couleur de mon élèment depuis que j'ai passer mon carré en get element by classname, pas ID,
+ et en utilisant "fill" */
+
+TweenMax.to(whiteLine, 5, {
+    color: "black",
+    backgroundColor: "black",
+    fill: "blue",
+
 });
 
-
+TweenMax.to(test2, 5, {
+    // x: -200,
+    // rotation: 100,
+    fill: "blue"
+});
 
 
 // //tween the element with ID of "myID"
