44658d1ad707d4a2d7c1f9b17a11fc93cb15bffc
Z-dorf <sebastian.zanon2@gmail.com>
2018-05-03 09:42:52 +0200

    adding html, css and js

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+var backGround = document.getElementById("BackGround");
+
+TweenMax.to(".background", 5, {
+    x: 600,
+    rotation: 100,
+    backgroundColor: "purple"
+});
+
+
+
+
+// //tween the element with ID of "myID"
+// TweenMax.to(".blacky", 5, {
+//     width: 150,
+//     height: 150,
+//     backgroundColor: "red"
+// });
+
+// TweenMax.to(".yellow", 6, {
+//     x: 600,
+//     backgroundColor: "#1D4A79",
+//     rotation: "-1080"
+// });
+
+// TweenMax.to(".blue", 6, {
+//     x: 600,
+//     rotation: 1080,
+//     scale: 0.5
+// });
+
+TweenMax.to(".rotativeSquare", 4, {
+    x: 600,
+    y: 250,
+    rotation: 3600,
+    scale: 2,
+    backgroundColor: "blue",
+    backgroundColor: "yellow",
+    backgroundColor: "purple"
+});
